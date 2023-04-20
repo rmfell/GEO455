d286b863228532542de4b8bb71aaa3eaea7e3eae
Ryan Fell <93163104+rmfell@users.noreply.github.com>
2023-04-20 16:38:58 -0500

    Add files via upload

--- lab_9/map_lab9.js
+++ lab_9/map_lab9.js
@@ -1,6 +1,7 @@
 var mymap = L.map('map', {
     center: [43.0672902307, -89.418123],
     zoom: 7,
+    layer: grayscale,
 });
 
 var grayscale = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
@@ -9,30 +10,39 @@ var grayscale = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/
     minZoom: 2,
 }).addTo(mymap);
 
+var counties = L.geoJson(poly, {
+     style: function (feature) {
+        return {fillOpacity: 0.7, weight: 2.5, color: feature.properties.color};
+     },
+   onEachFeature: function(feature, featureLayer) {
+        featureLayer.bindTooltip(feature.properties.NAME, {permanent: false, direction: 'right'});
+    }
+}).addTo(mymap);
+
 var migrationLayer = new L.migrationLayer({
     map: mymap,
     data: data,
     pulseRadius:30,
     pulseBorderWidth:3,
     arcWidth:1,
-    arcLabel:true,
+    arcLabel:false,
     arcLabelFont:'10px sans-serif',
-    maxWidth:10,
+    maxWidth:10
 });
 
-L.migrationLayer.addTo(mymap);
+migrationLayer.addTo(mymap);
 
-$(document).ready(function() {
-    $("#btn1").click(function(){
-        migrationLayer.hide();
-    });
-    $("#btn2").click(function(){
-        migrationLayer.show();
-    });
-    $("#btn3").click(function(){
-        migrationLayer.pause();
-    });
-    $("#btn4").click(function(){
-        migrationLayer.play();
-    });
-});
+mymap.fitBounds(counties.getBounds());
+
+function hide(){
+    migrationLayer.hide();
+}
+function show(){
+    migrationLayer.show();
+}
+function play(){
+    migrationLayer.play();
+}
+function pause(){
+    migrationLayer.pause();
+}
